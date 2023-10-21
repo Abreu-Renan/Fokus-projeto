59102960e473ab0e93cb08685270bb402fc5da4c
ProGameArcade <133807718+Abreu-Renan@users.noreply.github.com>
2023-10-20 23:19:54 -0300

    finalização projeto

--- script.js
+++ script.js
@@ -4,12 +4,41 @@ const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo  = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
+const musicaFocoImput = document.querySelector('#alternar-musica')
+const iniciarouPausarBt = document.querySelector('#start-pause span')
+const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-butto-icon") 
+const tempoNaTela = document.querySelector('#timer')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav')
+const audioPause = new Audio('/sons/pause.mp3')
+const audiotempoFinalizado = new Audio('/sons/beep.mp3')
 
+let tempoDecorridoEmSegundos = 1500
+let intervaloId = null
+
+musica.loop = true
+
+//tocar musica
+musicaFocoImput.addEventListener('change', () =>{
+    if(musica.paused){
+        musica.play()
+    }else{
+        musica.pause()
+    }
+})
 
 //troca de cor das paginas e troca de imagem
 function alterarContexto(contexto){
+    
+    mostrarTempo()
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
+
     switch(contexto){
         case "foco":
             titulo.innerHTML = ` Otimize sua produtividade,<br>
@@ -30,16 +59,59 @@ function alterarContexto(contexto){
 }
 
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500
    alterarContexto('foco') 
+   focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 } )
 
+//temporizador
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0){
+        //alert('tempo finalizado')
+        zerar()
+        return
+    }
+ 
+    tempoDecorridoEmSegundos -= 1
+    mostrarTempo()
+}
 
-//elteração de texto
+startPauseBt.addEventListener ('click', iniciarouPausar) 
+
+function iniciarouPausar(){
+    if(intervaloId){
+        audioPause.play()
+        zerar()
+        return
+    }
+    audioPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000)
+    iniciarouPausarBt.textContent = "Pausar"
+    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/pause.png`)
+}
+
+function zerar(){
+    clearInterval(intervaloId)
+    iniciarouPausarBt.textContent = "Começar"
+    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/play_arrow.png`)
+    intervaloId = null
+}
+
+function mostrarTempo(){
+    const tempo = new Date(tempoDecorridoEmSegundos*1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+mostrarTempo()
